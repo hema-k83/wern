42a7dcdcb981089e64bbbb2dc0b55bc777b8df64
Hema <hemak83@gmail.com>
2025-08-04 23:48:14 +0530

    Changed App signing to release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,7 +52,16 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            getByName("release") {
+                isMinifyEnabled = true
+                isShrinkResources = true
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+                signingConfig = signingConfigs.findByName("release")
+            }
+
         }
     }
 }
